04dc65e769de4c85625adf3dc55bd505a32ec1bc
Reformed Scribe <stmiller57@gmail.com>
2020-05-09 10:13:12 -0400

    rearranged code in server.js file

--- server.js
+++ server.js
@@ -11,30 +11,17 @@ const PORT = process.env.PORT || 8080;
 // Parse data with the Express app
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("public"));
 
 // HTML routes
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"))
-});
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"))
 });
 
-// API routes
-const data = JSON.parse(fs.readFileSync("db/db.json"));
-
-app.get("/api/notes", (req, res) => {
-    res.json(data);
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"))
 });
-
-app.post("/api/notes", (req, res) => {
-    data.push(req.body);
-    res.json(true);
-});
-
-app.delete("api/notes/:id");
 
 // Listener
 app.listen(PORT, () => {
