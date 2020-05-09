c2c14bc9026d09c29a2485c6e2a3b8b746da8c6c
Reformed Scribe <stmiller57@gmail.com>
2020-05-09 05:37:24 -0400

    Wrote HTML routes

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
-// Require in express and path, choose a port for connection
+// Require in Express and path
 const express = require("express");
-const app = express();
-const PORT = 8080;
 const path = require("path");
+
+// Set up Express app
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 
 // Parse data with the Express app
-// =============================================================
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname, "/public"));
 
+// HTML routes
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "assets/js/index.html"))
 
+});
 
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "assets/js/notes.html"))
 
+});
 
-// Route definitions
-app.get("/")
-
+// API routes
+app.get("/api/notes", (req, res) => {
+    res.readFile(db.JSON);
+});
 
 // Listener
 app.listen(PORT, function () {
