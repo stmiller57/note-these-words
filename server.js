7b524a5fa5fda416eb529a7c239eed6282be6f70
Reformed Scribe <stmiller57@gmail.com>
2020-05-08 21:42:24 -0400

    Add const variables and listener port to server.js file

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
+// Require in express and path, choose a port for connection
 const express = require("express");
 const app = express();
+const PORT = 8080;
+const path = require("path");
+
+
+// Parse data with the Express app
+// =============================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+// Route definitions
+app.get("/")
+
+
+// Listener
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
